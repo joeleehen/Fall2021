a62dcdfe71dc5b1254197ab38cfa620eb0d873cd
Joseph <joeylhendrix@gmail.com>
2021-08-26 23:14:15 -0500

    added DNA HW

---
+++ DNA.py
@@ -0,0 +1,16 @@
+import sys
+def main():
+    # read the data
+    fileName = sys.argv[1]
+    DNAfile = open(fileName, "r")
+    
+    # for each pair
+    # call longest_subsequence
+
+    # write out result(s)
+
+    # insert blank line
+
+
+if __name__ == "__main__":
+    main()
