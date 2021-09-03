1b432dbe58fc3ef760c5222f273d156d46539aef
Joseph Hendrix <joeylhendrix@gmail.com>
2021-09-02 19:04:52 -0500

    changes to WordSearch

--- WordSearch.py
+++ WordSearch.py
@@ -12,7 +12,10 @@ def getGridList(gridSize):
     for i in range(gridSize):
         rowStr = file.readline()
         rowStr = rowStr.replace("\n", "")    # remove escape character
-        rowLst = [rowStr]
+        rowLst = []
+        for c in rowStr:    # create list containing letters in grid row, omit spaces
+            if c != " ":
+                rowLst.append(c)
         gridList.append(rowLst)
     return gridList
 
@@ -33,14 +36,62 @@ file.readline()    # skip over blank line again
 wordList = getWorkBank()
 
 
+def horiSearch(word):
+    location = (0, 0)
+    for i in range(len(gridList)):
+        rowExamined = gridList[i]    # current row of grid being examined, is a list of characters!
+        rowJoined = "".join(rowExamined)    # convert to string
+        if rowJoined.find(word) != -1:
+            location = (i + 1, rowJoined.find(word) + 1)
+        else:
+            continue
+    return location
+
+
+def reverseHoriSearch(word):
+    location = (0, 0)
+    for i in range(len(gridList)):
+        rowExamined = gridList[i]
+        rowJoined = "".join(rowExamined)
+        wordReversed = word[::-1]
+        if rowJoined.find(wordReversed) != -1:
+            location = (i + 1, rowJoined.find(wordReversed) + len(wordReversed))
+        else:
+            continue
+    return location
+
+def vertSearch(word):
+    location = (0, 0)
+    for i in range(len(gridList)):
+        rowTrack = i    # used to iterate rows after initial match is found
+        rowExamined = gridList[i]
+        for j in range(len(rowExamined)):
+            if rowExamined[j] == word[0]:
+                rowFound = i    # denotes first row where word is found
+                wordTrack = 0    # used to iterate within the searchword
+                charFound = ''    # used to keep track of characters that match searchword
+                while wordTrack < len(word) and gridList[rowTrack][j] == word[wordTrack]:
+                    charFound += word[wordTrack]
+                    rowTrack += 1
+                    wordTrack += 1
+                    location = (i + 1, j + 1)
+                    if charFound == word:
+                        break
+                else:
+                    continue
+                break    # break out of while loop
+            else:
+                continue
+        else:
+            continue
+        break    # break out of row iterator
+    return location
+
+
+print(vertSearch("BOOTS"))
+
 # HERE'S THE PLAN
 # Each search function is based off of the first letter in the searchword.
-
-# Horizontal search is straightforward: for each word, look for that word
-# in each row. Could even use .find()
-
-# Vertical search is a bit harder. If word[0] is found at gridRow[0], go to the
-# next row at the same index and see if it has word[1].
 
 # Diagonal search is a combination of the two. Use indexing and for loops to iterate
 # through each row and index.
